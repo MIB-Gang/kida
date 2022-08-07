90079a3a0e60304833dde0d7e44b15d99d2de165
Sham3 <Sham3@Shamshomeh>
2022-08-07 14:26:51 +0200

    TODOS

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,61 @@
 package application;
 
-public class Controller {
+import java.util.ArrayList;
+import java.util.List;
 
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class Controller {
+	
+	//TODO ALLES
+	
+	List<Song> allSongs = new ArrayList<>();
+	//TODO Filepath wird benötigt nur, um die Lieder abzuspielen
+	
+	public void playPlaylist(MouseEvent event) {
+		
+	}
+
+	public void playSong(MouseEvent event, Song song) {
+		Media media = new Media (song.getAudioFilePath());
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.play();
+
+	}
+	
+	public void pause() {
+		
+	}
+	
+	public void search() {
+		
+	}
+	
+	public void sort() {
+		
+	}
+	
+	public void addLibrary() {
+		
+	}
+	
+	public void changeDate() {
+		
+	}
+	
+	public void deleteLibrary() {
+		
+	}
+	
+	public void generatePlaylist() {
+		
+	}
+	
+	public void createPlaylist() {
+		
+	}
+	
+	//TODO seekbar, volume
 }
