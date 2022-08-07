d00f8d081173b0505624bb8f5d7222b82c9a74c5
Tim Friedrich <tim.david007@gmail.com>
2022-08-07 18:56:07 +0200

    buttons dynamic

--- src/application/nodes/Sidemenu.java
+++ src/application/nodes/Sidemenu.java
@@ -1,6 +1,11 @@
 package application.nodes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import application.canvas.HomeCanva;
+import application.canvas.LibraryCanva;
+import application.canvas.SearchCanva;
 import application.controller.SceneController;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -12,22 +17,35 @@ import javafx.stage.Stage;
 
 public class Sidemenu extends VBox {
 	
-	public Sidemenu(Stage stage) {		
+	private Map<String, Node> buttons = new HashMap<>();
+	
+	public Sidemenu() {	
+		this.setId("sidemenu");
+
 		Image logo = new Image("kida.png");
 		
 		ImageView logoContainer = new ImageView();
 		logoContainer.setImage(logo);
 		logoContainer.setFitWidth(128);
 		logoContainer.setPreserveRatio(true);
-				
-		Button home = new Button("Home");
-		Button search = new Button("Suchen");
-		Button library = new Button("Bibliothek");
-		//home.setOnAction((e) -> { NavigationController.changeCanva(stage, new HomeCanva()); });
 		
-		this.setId("sidemenu");
-		this.getChildren().add(logoContainer);
-		this.getChildren().addAll(new Rectangle(0, 32), home, search, library);
+		this.getChildren().addAll(logoContainer, new Rectangle(0, 32));
+		
+		buttons = Map.of(
+				"Home", new HomeCanva(),
+				"Suchen", new SearchCanva(),
+				"Bibliothek", new LibraryCanva()
+		);
+		
+		for (String name: buttons.keySet()) {
+			Button menuButton = new Button(name);
+			menuButton.setOnAction((e) -> System.out.println(name));
+			SceneController controller = new SceneController();
+			//menuButton.setOnAction((e) -> controller.changeCanva(e, new LibraryCanva()));
+			//home.setOnAction((e) -> { NavigationController.changeCanva(stage, new HomeCanva()); });
+			this.getChildren().add(menuButton);
+		}
+		
 	}
 	
 }
