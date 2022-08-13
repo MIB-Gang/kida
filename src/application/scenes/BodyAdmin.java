403b4efbeee4ae2ab4c785a3ca03ea662f1a280d
Tim David Friedrich <timdavidfriedrich@gmail.com>
2022-08-13 12:25:23 +0200

    table moved to SongTable.java

--- src/application/scenes/BodyAdmin.java
+++ src/application/scenes/BodyAdmin.java
@@ -1,22 +1,12 @@
 package application.scenes;
 
-import application.Song;
 import application.controller.MediaController;
 import application.controller.PlayerController;
 import application.controller.SceneController;
 import application.uiComponents.DefaultButton;
 import application.uiComponents.SongTable;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 public class BodyAdmin extends VBox {
@@ -28,10 +18,7 @@ public class BodyAdmin extends VBox {
 	private SongTable table = new SongTable();
 	
 	private DefaultButton importButton = new DefaultButton("Importieren");
-	
-    ContextMenu cm = new ContextMenu();
-    MenuItem mi = new MenuItem("Zu Playlist hinzufuegen");
-    MenuItem mi1 = new MenuItem("Umbenennen");
+		
 	
 	public BodyAdmin() {
 
