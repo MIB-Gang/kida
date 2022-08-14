d5023609c3f1f834dd8b8c86649a224b9e4ad2b6
Tim David Friedrich <timdavidfriedrich@gmail.com>
2022-08-14 20:47:12 +0200

    search ui angefangen

--- src/application/scenes/BodySearch.java
+++ src/application/scenes/BodySearch.java
@@ -1,12 +1,24 @@
 package application.scenes;
 
-import javafx.scene.control.Label;
+import application.uiComponents.DefaultTextField;
+import application.uiComponents.SongTable;
+import javafx.geometry.Insets;
 import javafx.scene.layout.VBox;
 
 public class BodySearch extends VBox {
 	
+	DefaultTextField searchField = new DefaultTextField();
+	SongTable table = new SongTable();
+	
 	public BodySearch() {
-		this.getChildren().add(new Label("Search"));
+		this.setSpacing(12);
+		this.getChildren().addAll(searchField, table);
+		
+		applyStyle();
+	}
+	
+	private void applyStyle() {
+		this.setPadding(new Insets(31.25, 32, 0, 32));
 	}
 
 }
