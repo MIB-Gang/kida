559c962d4ba74bbf8f9a1382075658f49032f73c
Tim David Friedrich <timdavidfriedrich@gmail.com>
2022-08-16 19:19:37 +0200

    playlist nameField

--- src/application/scenes/BodyPlaylist.java
+++ src/application/scenes/BodyPlaylist.java
@@ -5,6 +5,7 @@ import application.controller.MediaController;
 import application.uiComponents.KTextField;
 import application.uiComponents.KBackButton;
 import application.uiComponents.KSongTable;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -22,21 +23,36 @@ public class BodyPlaylist extends VBox {
 	private HBox header = new HBox();
 	private KBackButton backButton = new KBackButton();
 	private KTextField nameField = new KTextField();
-	private ImageView editIcon = new ImageView();
 	private KSongTable table = new KSongTable();
 	
 	public BodyPlaylist(Playlist playlist) {
 		
-		nameField.setPromptText("Playlist-Namen eingeben");
 		nameField.setText(playlist.getName());
 		
-		header.getChildren().addAll(backButton, nameField, editIcon);
+		Platform.runLater(new Runnable() {
+		    @Override
+		    public void run() {
+		    	if (nameField.getText().contains("Neue Playlist")) {
+		    		nameField.requestFocus();
+		    		nameField.selectAll();
+		    	}
+		    	if (mediaController.getArtistPlaylists().contains(playlist) ||
+		    		mediaController.getAlbumPlaylists().contains(playlist) ||
+		    		mediaController.getGenrePlaylists().contains(playlist)) {
+			    	nameField.setEditable(false);
+		    	}
+		    }
+		});
+				
+		header.getChildren().addAll(backButton, nameField);
 		this.getChildren().addAll(header, table);
 		
 		table.setItems(playlist.getSongs());
 	
 		nameField.textProperty().addListener((observableText, oldText, newText) -> {
 			playlist.setName(newText);
+			int maxChars = 16;
+			if (nameField.getText().length() > maxChars) nameField.setText(nameField.getText().substring(0, maxChars));
 		});
 		
 		
@@ -46,16 +62,13 @@ public class BodyPlaylist extends VBox {
 	private void applyStyle() {
 		this.setPadding(new Insets(31.25, 32, 0, 32));
 		this.setSpacing(12);
-		
-		editIcon.setImage(new Image("/edit.png"));
-		editIcon.setFitWidth(24);
-		editIcon.setPreserveRatio(true);
-		
+				
 		header.setSpacing(12);
 		header.setAlignment(Pos.CENTER_LEFT);
-		HBox.setHgrow(nameField, Priority.ALWAYS);
 		
+		nameField.setPromptText("Playlist-Namen eingeben");
 		nameField.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-text-fill: white; -fx-font-weight: bold; -fx-font-size: 36; -fx-padding: 0;");
+		HBox.setHgrow(nameField, Priority.ALWAYS);
 	}
 
 }
